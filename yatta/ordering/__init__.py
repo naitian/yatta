aabc4ed4b639ce02397a619e9b386ed61f9fdd44
Naitian Zhou <naitian@naitian.org>
2024-07-19 21:17:00 -0700

    move code from ordering to ordering.server

--- yatta/ordering/__init__.py
+++ yatta/ordering/__init__.py
@@ -1,6 +1,5 @@
 """Data ordering classes"""
 
-
 class DataOrdering:
     """Base class for orderings
 
@@ -38,37 +37,3 @@ class RandomOrdering(DataOrdering):
         random.shuffle(indices)
         for idx in indices:
             yield self.assignments[idx]
-
-
-def sliding_window(seq, n):
-    """Returns a sliding window (of width n) over data from the iterable"""
-    it = iter(seq)
-    result = tuple(itertools.islice(it, n))
-    if len(result) == n:
-        yield result
-    for elem in it:
-        result = result[1:] + (elem,)
-        yield result
-
-
-def assign_ordering(ordering, assignments, session):
-    """Assign an ordering to a list of assignments"""
-    for idx, (prev, curr, next) in enumerate(
-        sliding_window(itertools.chain([None], ordering(assignments), [None]), 3)
-    ):
-        curr.rank = idx
-        curr.prev = prev.datum_id if prev is not None else None
-        curr.next = next.datum_id if next is not None else None
-    session.commit()
-
-
-def assign_all_orderings():
-    with Session(engine) as session:
-        users = session.exec(select(User)).all()
-        for user in users:
-            assignments = session.exec(
-                select(AnnotationAssignment).where(
-                    AnnotationAssignment.user_id == user.id
-                )
-            ).all()
-            assign_ordering(settings.ordering, assignments, session)
