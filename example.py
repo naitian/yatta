1b5c9c1cf61c5be2502badc12ca283bb032f1e3c
Naitian Zhou <naitian@naitian.org>
2024-09-24 12:01:37 -0700

    Add example

---
+++ example.py
@@ -0,0 +1,24 @@
+from yatta.core import Yatta
+from yatta.web import Server
+from yatta.distributor import AllDistributor
+from yatta.base.textbox import Textbox
+
+yatta = Yatta(
+    dataset=[
+        {"text": "Hello, world!"},
+        {"text": "Goodbye, world!"},
+        {"text": "Hello, again!"},
+        {"text": "Goodbye, again!"},
+        {"text": "Hello, one more time!"},
+    ],
+    task={"text": Textbox(transform_fn=lambda x: x["text"])},
+    distributor=AllDistributor,
+    ordering=iter,
+)
+
+with yatta.session():
+    yatta.assign_tasks()
+    yatta.assign_all_orderings()
+
+server = Server(yatta, port=5000, secret_key="very_secret_key", dev=True)
+server.run()
